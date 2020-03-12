c254698039a56012082b357030e60a789fc6aded
sanhhn <sanhhn@gmail.com>
2020-03-12 15:04:16 -0400

    cc

--- hrm/e2e/src/app.e2e-spec.ts
+++ hrm/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to hrm!');
+    expect(page.getTitleText()).toEqual('hrm app is running!');
   });
 
   afterEach(async () => {
