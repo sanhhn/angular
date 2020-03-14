467afe88963bcac5afac561977cd63288861de57
sanhhn <sanhhn@gmail.com>
2020-03-13 22:26:55 -0400

    mac

---
+++ Employee1/EmpApi/Controllers/EmployeeAPIController.cs
@@ -0,0 +1,11 @@
+using EmpApi.Model;
+using System.Web.Http; 
+
+namespace EmpApi.Controllers
+{
+    [RoutePrefix("Api/Employee")] 
+    public class EmployeeAPIController: ApiController  
+    {
+        
+    }
+}
